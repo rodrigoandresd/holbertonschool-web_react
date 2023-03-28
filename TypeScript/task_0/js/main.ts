971ba3b0d92bf08e958290359c14537ac240b313
rodrigoandresd <rodrigoaduqueg@gmail.com>
2023-03-28 17:02:47 -0500

    task3 - task1

--- TypeScript/task_0/js/main.ts
+++ TypeScript/task_0/js/main.ts
@@ -31,3 +31,10 @@ const director1: Directors = {
 
 console.log(director1);
 
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string,
+}
+
+export const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName[0]}, ${lastName}`;
+}
