aae27ca608ac1bba0ff861632a7adddefef0ef97
rodrigoandresd <rodrigoaduqueg@gmail.com>
2023-03-28 17:20:49 -0500

    task4 - task1

--- TypeScript/task_0/js/main.ts
+++ TypeScript/task_0/js/main.ts
@@ -38,3 +38,31 @@ interface printTeacherFunction {
 export const printTeacher: printTeacherFunction = (firstName, lastName) => {
   return `${firstName[0]}, ${lastName}`;
 }
+
+export interface StudentConstructor {
+  new(firstName: string, lastName: string): StudentClassInterface;
+}
+
+export interface StudentClassInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+export class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
