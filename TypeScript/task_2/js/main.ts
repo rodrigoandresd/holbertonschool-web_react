68c10b06d9d173681d2ee9bcd3f02b6d9de359b1
rodrigoandresd <rodrigoaduqueg@gmail.com>
2023-03-29 15:23:34 -0500

    task 5 - task 2

---
+++ TypeScript/task_2/js/main.ts
@@ -0,0 +1,47 @@
+interface DirectorInterface {
+  workFromHome(): string
+  getCoffeeBreak(): string
+  workDirectorTasks(): string
+}
+
+interface TeacherInterface {
+  workFromHome(): string
+  getCoffeeBreak(): string
+  workTeacherTasks(): string
+}
+
+export const Director = class Director implements DirectorInterface {
+  workFromHome(): string {
+    return 'Working from home';
+  }
+
+  getCoffeeBreak(): string {
+    return 'Getting a coffee break';
+  }
+
+  workDirectorTasks(): string {
+    return 'Getting to director tasks';
+  }
+}
+
+export class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return 'Cannot work from home'
+  }
+
+  getCoffeeBreak(): string {
+    return 'Cannot have a break'
+  }
+
+  workTeacherTasks(): string {
+    return 'Getting to work'
+  }
+}
+
+export function createEmployee(firstName: string, lastName: string, salary: number | string): DirectorInterface | TeacherInterface {
+  if (salary < 500) {
+    return new Teacher();
+  }
+
+  return new Director();
+}
