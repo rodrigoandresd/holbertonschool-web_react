0542fafde64aeb41857fdde5ce2e0c5301623ebf
rodrigoandresd <rodrigoaduqueg@gmail.com>
2023-03-30 07:06:51 -0500

    Task 10 - task 4

---
+++ TypeScript/task_4/js/main.ts
@@ -0,0 +1,24 @@
+export const cpp: Subjects.Cpp = new Subjects.Cpp();
+export const java: Subjects.Java = new Subjects.Java();
+export const react: Subjects.React = new Subjects.React();
+
+const cTeacher = {
+  firstName: 'Guillaume',
+  lastName: 'Salva',
+  experienceTeachingC: 10
+}
+
+console.log('C++');
+cpp.teacher = cTeacher;
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+console.log('Java');
+java.teacher = cTeacher;
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+console.log('React');
+react.teacher = cTeacher;
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
